decb788ebf3398305035b9b37073b7caf0ebee0b
aisyahaprilia154 <aisyahaprilia1515@gmail.com>
2026-04-02 21:19:35 +0700

    fixing word

--- Controllers/Manager/DashboardController.php
+++ Controllers/Manager/DashboardController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Transaction;
 use App\Models\Supply;
-use App\Models\Produc;
+use App\Models\Product;
 use App\Models\Restock;
 use App\Models\PasswordResetRequest;
 class DashboardController extends Controller
