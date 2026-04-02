095d546ba8a940daccf339ff43b3dca8f9f3b2e8
aisyahaprilia154 <aisyahaprilia1515@gmail.com>
2026-04-02 21:18:02 +0700

    add model imports for dashboard

--- Controllers/Manager/DashboardController.php
+++ Controllers/Manager/DashboardController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+use App\Models\Supply;
+use App\Models\Produc;
+use App\Models\Restock;
+use App\Models\PasswordResetRequest;
 class DashboardController extends Controller
 {
 }
