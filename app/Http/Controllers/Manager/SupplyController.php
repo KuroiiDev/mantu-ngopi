ce420e741cfbe4938ba6abfe0530195389c0a98e
Alex-afa <alexispratamabahar2006@gmail.com>
2026-04-05 22:18:12 +0700

    CategoryController.php

---
+++ app/Http/Controllers/Manager/SupplyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+use App\Models\Supply;
+use App\Models\Product;
+use App\Models\Restock;
+use App\Models\PasswordResetRequest;
+class SupplyController extends Controller
+{
+    public function index()
+{
+        $today = $this->todaySummary();
+        $weeklySale = $this->weeklySale();
+        $lowStock = $this->lowStock();
+        $emptyStock = $this->emptyStock();
+        $totalProduct = Product::count();
+        $recentTransaction = Transaction::with('user')->latest()->limit(5)->get();
+        $topProduct = $this->topProduct();
+}
