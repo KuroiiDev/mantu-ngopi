f5ce82cc8790d5dc6b79466200eec612c0b585d5
aisyahaprilia154 <aisyahaprilia1515@gmail.com>
2026-04-10 15:28:27 +0700

    add product controller dependencies

--- Controllers/Manager/ProductController.php
+++ Controllers/Manager/ProductController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
 
+ use App\Models\Product;
+use App\Models\Category;
+use App\Models\Supply;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 class ProductController extends Controller
 {
 }
