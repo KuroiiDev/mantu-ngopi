025a63b82db377cbc20164c374d27eb2371fd55f
aisyahaprilia154 <aisyahaprilia1515@gmail.com>
2026-04-11 10:57:30 +0700

    add transaction model import

--- Controllers/Manager/TransactionController.php
+++ Controllers/Manager/TransactionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
 
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
 class TransactionController extends Controller 
 {
 }
