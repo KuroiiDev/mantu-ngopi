917288a85a312741bd310e1810ee1bcd7a77e246
aisyahaprilia154 <aisyahaprilia1515@gmail.com>
2026-04-11 10:58:17 +0700

    add basic index method

--- Controllers/Manager/TransactionController.php
+++ Controllers/Manager/TransactionController.php
@@ -9,4 +9,9 @@ use Illuminate\Http\Request;
 
 class TransactionController extends Controller 
 {
+    public function index()
+{
+    $transactions = Transaction::all();
+    return view('manager.transactions.index', compact('transactions'));
+}
 }
